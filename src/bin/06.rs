27bfc69de1b7a32d8e52238d16f57c5a0c8c272d
anothergenz <git@angz.dev>
2024-12-06 16:40:35 -0500

    day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,8 +1,9 @@
+use rayon::prelude::*;
 use std::collections::HashSet;
 
 advent_of_code::solution!(6);
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum Orientation {
     Up,
     Down,
@@ -40,7 +41,7 @@ fn is_open(grid: &Vec<&[u8]>, x: usize, y: usize) -> bool {
     true
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Position {
     x: usize,
     y: usize,
@@ -98,28 +99,33 @@ impl TryFrom<(usize, usize, &u8)> for Position {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let grid = input
+fn parse_grid(input: &str) -> Vec<&[u8]> {
+    input
         .trim()
         .lines()
         .map(|line| line.as_bytes())
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    let max_x = grid[0].len();
-    let max_y = grid.len();
-
-    let mut current_pos: Position = grid
-        .iter()
+fn get_start_pos(grid: &Vec<&[u8]>) -> Position {
+    grid.iter()
         .enumerate()
         .find_map(|(r_idx, row)| {
             row.iter()
                 .enumerate()
                 .find_map(|(c_idx, char)| (c_idx, r_idx, char).try_into().ok())
         })
-        .expect("Guard missing??");
+        .expect("Guard missing??")
+}
 
+fn get_guard_positions(grid: &Vec<&[u8]>) -> (Position, HashSet<(usize, usize)>) {
+    let max_x = grid[0].len();
+    let max_y = grid.len();
+
+    let mut current_pos = get_start_pos(&grid);
+    let starting_pos = current_pos;
     let mut guard_positions: HashSet<(usize, usize)> =
-        HashSet::from([(current_pos.x, current_pos.y)]);
+        HashSet::from([(starting_pos.x, starting_pos.y)]);
 
     loop {
         current_pos.step(&grid);
@@ -131,11 +137,56 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
+    (starting_pos, guard_positions)
+}
+
+fn is_loop(grid: &Vec<&[u8]>, starting_pos: &Position) -> bool {
+    let max_x = grid[0].len();
+    let max_y = grid.len();
+
+    let mut current_pos = *starting_pos;
+    let mut guard_positions: HashSet<(usize, usize, Orientation)> =
+        HashSet::from([(current_pos.x, current_pos.y, current_pos.orientation)]);
+
+    loop {
+        current_pos.step(&grid);
+
+        if (0..max_x).contains(&current_pos.x) && (0..max_y).contains(&current_pos.y) {
+            if !guard_positions.insert((current_pos.x, current_pos.y, current_pos.orientation)) {
+                return true;
+            }
+        } else {
+            break;
+        }
+    }
+
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = parse_grid(input);
+    let (_, guard_positions) = get_guard_positions(&grid);
+
     Some(guard_positions.len() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = parse_grid(input);
+    let (starting_position, guard_path) = get_guard_positions(&grid);
+
+    Some(
+        guard_path
+            .par_iter()
+            .filter(|(x, y)| {
+                let mut grid = grid.clone();
+                let mut new_row = Vec::from(grid[*y]);
+                new_row[*x] = b'#';
+                grid[*y] = &new_row;
+
+                is_loop(&grid, &starting_position)
+            })
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -151,6 +202,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
