faec4ad938f36a4c9612c50551c0748794e0cff6
anothergenz <git@angz.dev>
2024-12-07 14:04:58 -0500

    day 7 more efficient

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,6 +1,3 @@
-use rayon::prelude::*;
-use std::iter::successors;
-
 advent_of_code::solution!(7);
 
 #[derive(Debug)]
@@ -18,11 +15,42 @@ impl Operator {
             Add => l + r,
             Multiply => l * r,
             Concatenate => {
-                let num_r_digits = successors(Some(r), |&n| (n >= 10).then(|| n / 10)).count();
+                let num_r_digits = r.checked_ilog10().unwrap_or(0) + 1;
 
-                l * 10_u64.pow(num_r_digits as u32) + r
+                l * 10_u64.pow(num_r_digits) + r
             }
         }
+    }
+}
+
+trait Digits<T> {
+    fn num_digits(&self) -> u32;
+    fn get_first_digits(&self, n_remove: u32) -> T;
+    fn compare_digits(&self, other: T, n: u32) -> bool;
+}
+
+impl Digits<u64> for u64 {
+    fn num_digits(&self) -> u32 {
+        self.checked_ilog10().unwrap_or(0) + 1
+    }
+    fn get_first_digits(&self, n_remove: u32) -> u64 {
+        let mask = 10u64.pow(n_remove);
+        (self - (self % mask)) / mask
+    }
+
+    fn compare_digits(&self, mut other: u64, n: u32) -> bool {
+        let mut l = *self;
+
+        for _ in 0..n {
+            if (l % 10) != (other % 10) {
+                return false;
+            }
+
+            l /= 10;
+            other /= 10;
+        }
+
+        true
     }
 }
 
@@ -32,29 +60,40 @@ fn solvable(test_value: u64, operands: &mut [u64], operators: &[Operator]) -> bo
     if num_operands == 2 {
         return operators
             .iter()
-            .any(|op| op.apply(operands[0], operands[1]) == test_value);
+            .any(|op| op.apply(operands[1], operands[0]) == test_value);
     }
 
     operators.iter().any(|op| {
         let first = operands[0];
-        let second = operands[1];
         let operands = &mut operands[1..num_operands];
-        operands[0] = op.apply(first, second);
 
-        if solvable(test_value, operands, operators) {
-            return true;
-        }
+        let new_test_value = match op {
+            Operator::Add => test_value - first,
+            Operator::Multiply => {
+                if test_value % first != 0 {
+                    return false;
+                }
 
-        operands[0] = second;
+                test_value / first
+            }
+            Operator::Concatenate => {
+                let num_digits = first.num_digits();
 
-        false
+                if !test_value.compare_digits(first, num_digits) {
+                    return false;
+                }
+
+                test_value.get_first_digits(num_digits)
+            }
+        };
+
+        solvable(new_test_value, operands, operators)
     })
 }
 
 fn sum_results(input: &str, operators: &[Operator]) -> u64 {
     input
         .lines()
-        .par_bridge()
         .filter_map(|line| {
             let (val, nums) = line.split_once(":").expect("Missing colon");
             let test_value = val.parse::<u64>().unwrap();
@@ -64,11 +103,9 @@ fn sum_results(input: &str, operators: &[Operator]) -> u64 {
                 .map(|n| n.parse::<u64>().unwrap())
                 .collect::<Vec<_>>();
 
-            if solvable(test_value, &mut operands, operators) {
-                return Some(test_value);
-            }
+            operands.reverse();
 
-            None
+            solvable(test_value, &mut operands, operators).then_some(test_value)
         })
         .sum()
 }
